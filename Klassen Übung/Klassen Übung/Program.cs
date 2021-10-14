30e73aabe71e01e1650f3b02eb34756d6a9d1e20
basti <84239503+basti-debug@users.noreply.github.com>
2021-10-14 22:02:13 +0200

    Aufgabe A fertig
    Aufgabe A fertig gestellt B angefangen aber keine Ahnung wie man Collections ausliest

--- Klassen Übung/Klassen Übung/Program.cs
+++ Klassen Übung/Klassen Übung/Program.cs
@@ -7,11 +7,15 @@ namespace Klassen_Übung
     {
         static void Main(string[] args)
         {
-            Car bmw = new Car("bmw", "1er", 150);
+            
         }
     }
+
+    Car bmw = new Car("bmw", "1er", 150);
     class Car
     {
+
+
         private string brand;
         private string type;
         private int ps;
@@ -19,15 +23,11 @@ namespace Klassen_Übung
         public Car(string brand, string type, int ps)
         {
             this.brand = brand;
+            this.type = type;
             this.ps = ps;   
         }
 
-        public Car(string brand, string type, int ps)
-        {
-            this.brand = brand;
-            this.type = type;
-            this.ps = ps;
-        }
+       
 
     }
     
---
+++ ÜbungenKlassen/ÜbungenKlassen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ÜbungenKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
