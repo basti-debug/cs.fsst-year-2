d1315efb6da1cc434c6e5825d7b9dd3ab87f06e3
basti <84239503+basti-debug@users.noreply.github.com>
2021-11-03 00:29:50 +0100

    v1
    fetch number one tonight

---
+++ class training/Aufgabe 3 - Klassen/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_3___Klassen
+{
+   
+    public class person
+    {
+        public string Name;
+        public int Age;
+        public bool HasPet;
+
+        public void Greeting()
+        {
+            Console.WriteLine("Hey my name is " + Name + " and iam " + Age + " years old");
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person gustav = new person();
+            gustav.Name = "Gustav";
+            gustav.Age = 12;
+            gustav.HasPet = true;
+
+            gustav.Greeting();
+
+            Console.ReadKey();
+        }
+    }
+
+}
