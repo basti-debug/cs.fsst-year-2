71c28c5cfcc84b0f5633a15a5ecb8bfe5f42b6fc
basti <84239503+basti-debug@users.noreply.github.com>
2021-12-01 00:25:07 +0100

    Aufgabe 1 done
    v2 1-12-2021

---
+++ Testübung 2/Testübung 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testübung_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
