1c66918c28c96334aae889f10e1ff92cee4a5013
basti <84239503+basti-debug@users.noreply.github.com>
2022-01-29 14:46:02 +0100

    v0.1
    not started yet

---
+++ GradeCalculator/GradeCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using GradeCalculator;
+
+/* Grade Calculator 
+ * by basti 
+ * 
+ * Session from 19.1.2022
+ * 
+ * v0.1 
+ */
+
+class Program
+{
+
+    static void Main(string[]args)
+    {
+
+        List<subjectcon> allgrades = new List<subjectcon >();
+        int howmany = 4;
+        
+
+        Console.WriteLine("Grade Calculator:");
+        Console.WriteLine("version v0.1");
+        Console.WriteLine("");
+        Console.WriteLine("How many subjects do wana calculate?");
+
+        howmany = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.WriteLine("Please enter at least 4 subjects");
+        Console.WriteLine("Use the given syntax:");
+        Console.WriteLine("yourname, subject, grade");
+
+        while(howmany > 0)
+        {
+            try
+            {
+                string __name;
+                string __subject;
+                int __grade;
+
+                string entry = Console.ReadLine();
+                string[] _entry = entry.Split(",");
+
+                __name = _entry[0];
+                __subject = _entry[1];
+                __grade = Convert.ToInt32(_entry[2]);
+
+                subjectcon entry1 = new subjectcon(__name,__subject,__grade);
+                allgrades.Add(entry1);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid format");
+            }
+
+        }
+        
+
+    }
+}
---
+++ GradeCalculator/GradeCalculator/subjectcon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculator
+{
+    class subjectcon
+    {
+        public string name;
+        public string subjectname;
+        public int grade;
+        public subjectcon(string _name, string _subject,int _grade)
+        {
+            name=_name;
+            subjectname=_subject;
+            grade=_grade;
+        }
+        
+    }
+}
