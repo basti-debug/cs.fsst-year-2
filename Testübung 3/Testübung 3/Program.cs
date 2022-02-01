f8440f8d8bef2c3896302b428a71ddb933b32879
basti.debug <basti.debug@gmail.com>
2022-02-02 00:12:03 +0100

    v0.1

---
+++ Testübung 3/Testübung 3/Program.cs
@@ -0,0 +1,19 @@
+﻿/* by basti
+ * date: 1.2.2022
+ * 
+ * for exam2 
+ * 
+ * revision 0.1
+ */
+
+/* Sample Questions:
+ * 
+ * 1) Erkläre Polymorphismus:
+ * Antwort: Es gibt wie eine Blaupause und davon kann man mehrere verschiedene zb Menschen erstellen
+ * 
+ * 2) Nenne 3 Eigenschaften zum Keyword abstract für Klassen und Methoden
+ * Antwort: Man kann sie nicht inzialisieren, Kann Methoden und Accessoren haben, man kann nicht direkt auf sie zugreifen nur erbende können darauf zugreifen 
+ * 
+ * 3) Wie können wir sicherstellen, dass Methoden oder Properties nur für erbende Klassen sichtbar ist ?
+ * Antwort: Mit hilfe des Keywords protected ist es nur innerhalb der eigenen Klasse und seinen Erben Methoden und Properties zu sehen
+ */
