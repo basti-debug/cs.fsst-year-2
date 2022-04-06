37540a4ea03c19522c3c021b5d93699b13e37a61
basti <84239503+basti-debug@users.noreply.github.com>
2022-04-06 19:34:27 +0200

    fertig, hoffentlich

--- serialisation2/Program.cs
+++ serialisation2/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+
+namespace serialisation2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> productList = new List<Product>();
+            productList.Add(new Product("1", "Döner", 5.50, "Beschreibung", true));
+            productList.Add(new Product("2", "Pommes", 4, "Beschreibung", true));
+            productList.Add(new Product("3", "Dürüm", 6.50, "Beschreibung", true));
+            productList.Add(new Product("4", "Pizza", 8.50, "Beschreibung", true));
+            productList.Add(new Product("5", "Burger", 7, "Beschreibung", true));
+
+
+            // Speichern der Product-Objekte:
+            Save(productList, "student_list.csv");
+
+            // Laden gespeicherten Product-Objekte
+            List<Product> studentListLoaded = LoadList("student_list.csv");
+
+            Console.ReadKey();
+        }
+        static void Save(List<Product> productList, string path)
+        {
+            // Einen StreamWriter für die angegebene Datei öffnen
+            using (StreamWriter stream = new StreamWriter(path, append: false))
+            {
+                // Über alle Product-Objekte in der Liste iterieren
+                foreach (Product product in productList)
+                {
+                    // Serialisieren des aktuellen Objekts
+                    string serializedData = product.SerializeToCsvString();
+
+                    // Schreiben einer neuen Zeile in der Datei           
+                    stream.WriteLine(serializedData);
+                }
+            }
+        }
+
+        static List<Product> LoadList(string path)
+        {
+            List<Product> products = new List<Product>();
+
+            // Einen StreamReader für die angegebene Datei öffnen
+            using (StreamReader stream = new StreamReader(path))
+            {
+                // Weitermachen, solange die Datei nicht am Ende angekommen ist
+                while (!stream.EndOfStream)
+                {
+                    // Nächste Zeile aus der Datei einlesen
+                    string serializedData = stream.ReadLine();
+
+                    // Die Daten Deserialisieren            
+                    Product product = Product.DeserializeFromCsvString(serializedData);
+
+                    // Das geladene Objekt in die Liste speichern       
+                    products.Add(product);
+                }
+            }
+
+            return products;
+        }
+    }
+}
---
+++ serialisation2/product.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public class product
+{
+    public string Sku { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Description { get; set; }
+    public bool LimitedEdition { get; set; }
+
+    public Product(string sku, string name, double price, string description, bool limitedEdition)
+    {
+        Sku = sku;
+        Name = name;
+        Price = price;
+        Description = description;
+        LimitedEdition = limitedEdition;
+
+    }
+
+    public string SerializeToCsvString()
+    {
+        // Convert the properties to a ";" separated string
+        string serialized = $"{Sku};{Name};{Price};{Description};{LimitedEdition}";
+        return serialized;
+    }
+
+    public static Product DeserializeFromCsvString(string serialized)
+    {
+        // Split the string according to the used delimiter
+        string[] fields = serialized.Split(';');
+
+        // Create a new product using the provided data
+        Product product = new Product(fields[0], fields[1], double.Parse(fields[2]), fields[3], bool.Parse(fields[4]));
+        return product;
+    }
+
+    static void Save(Product product, string path)
+    {
+        // Open a new file stream to write into a text file
+        using (StreamWriter stream = new StreamWriter(path, append: false))
+        {
+            // Serialize the product
+            string serializedData = product.SerializeToCsvString();
+
+            // Write the data to the file        
+            stream.WriteLine(serializedData);
+        }
+    }
+
+    static Product Load(string path)
+    {
+        Product product = null;
+
+        // Load from file
+        using (StreamReader stream = new StreamReader(path))
+        {
+            // Get the first line of the text file (csv)
+            string serializedData = stream.ReadLine();
+
+            // Deserialize the string        
+            product = Product.DeserializeFromCsvString(serializedData);
+        }
+
+        return product;
+    }
+}
