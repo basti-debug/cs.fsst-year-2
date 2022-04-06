9533273e204fdcdbbc4e372f4bfc5771710584fe
basti.debug <basti.debug@gmail.com>
2022-04-06 16:50:35 +0200

    excressice for 1 april

---
+++ serialisation/product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace serialisation
+{
+	public class serialisation
+	{
+		public serialisation(string sku, string name, double price, string description, bool limited Edition)
+		{
+			string _sku = sku;
+			string _name = name;
+			double _price = price;
+			string _description = description;
+			bool _limitedEdition = limited;
+
+		}
+	}
+}
+
